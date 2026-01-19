1292d76b8e0c1ca43c461f51095dd77d194c3aee
Sriramlite <Sriramlite.2020.78@gmail.com>
2026-01-19 15:49:19 +0530

    Fix: Add missing .js extension for browser ESM support

--- public/js/capacitor-push/index.js
+++ public/js/capacitor-push/index.js
@@ -1,5 +1,5 @@
 import { registerPlugin } from '../capacitor-core.js';
 const PushNotifications = registerPlugin('PushNotifications', {});
-export * from './definitions';
+export * from './definitions.js';
 export { PushNotifications };
 //# sourceMappingURL=index.js.map
